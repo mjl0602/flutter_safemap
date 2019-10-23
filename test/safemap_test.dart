ff3f3bb3c841b2fbf7de68c004bf771e47ced834
mjl0602 <675531696@qq.com>
2019-10-23 16:57:45 +0800

    init

---
+++ test/safemap_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:safemap/safemap.dart';
+
+void main() {
+  test('get value from safemap', () {
+    // source map
+    Map map = {
+      'id': 3,
+      'tag': 'student',
+      'info': {
+        'name': 'Jerry',
+      },
+      'class': [
+        {
+          "name": 'class 1',
+          'tag': '',
+        },
+        {},
+      ],
+    };
+    SafeMap safeMap = SafeMap(map);
+    assert(safeMap['id'].value == 3);
+    assert(safeMap['id'].string == null);
+    assert(safeMap['tag'].value == 'student');
+    assert(safeMap['info']['name'].value == 'Jerry');
+    assert(safeMap['class'].list.length == 2);
+    assert(safeMap['class'][0]['name'].value == 'class 1');
+    assert(safeMap['class'][0]['tag'].isEmpty());
+    assert(safeMap['class'][1].isEmpty());
+    assert(safeMap['class'][2].isEmpty());
+  });
+}
