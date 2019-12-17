7d1af977e9ec340b4d35e146954e4935cca161ff
MJL <675531696@qq.com>
2019-12-17 13:24:48 +0800

    remove flutter

--- test/safemap_test.dart
+++ test/safemap_test.dart
@@ -1,47 +1,45 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
 import 'package:safemap/safemap.dart';
 
 void main() {
-  test('get value from safemap', () {
-    // source map
-    Map map = {
-      'id': 3,
-      'tag': 'student',
-      'info': {
-        'name': 'Jerry',
+  // source map
+  Map map = {
+    'id': 3,
+    'tag': 'student',
+    'info': {
+      'name': 'Jerry',
+    },
+    'class': [
+      {
+        "name": 'class 1',
+        'tag': '',
       },
-      'class': [
-        {
-          "name": 'class 1',
-          'tag': '',
-        },
-        {},
-      ],
-    };
-    SafeMap safeMap = SafeMap(map);
+      {},
+    ],
+  };
+  SafeMap safeMap = SafeMap(map);
 
-    // 取值,错误的类型会返回SafeMap(null)
-    assert(safeMap['id'].value == 3);
-    assert(safeMap['id'].string == null);
+  // 取值,错误的类型会返回SafeMap(null)
+  assert(safeMap['id'].value == 3);
+  assert(safeMap['id'].string == null);
 
-    // 错误的下标也会返回SafeMap(null)
-    assert(safeMap['tag'].value == 'student');
-    assert(safeMap['tag12345'].value == null);
+  // 错误的下标也会返回SafeMap(null)
+  assert(safeMap['tag'].value == 'student');
+  assert(safeMap['tag12345'].value == null);
 
-    // 连续取值,可以一直安全取值,只会返回SafeMap(null).
-    assert(safeMap['info']['name'].value == 'Jerry');
-    assert(safeMap['a']['b']['b']['b']['b']['b'].value == null);
+  // 连续取值,可以一直安全取值,只会返回SafeMap(null).
+  assert(safeMap['info']['name'].value == 'Jerry');
+  assert(safeMap['a']['b']['b']['b']['b']['b'].value == null);
 
-    // 取出数组，也可以通过数组下标继续取值
-    assert(safeMap['class'].list.length == 2);
-    assert(safeMap['class'][0]['name'].value == 'class 1');
+  // 取出数组，也可以通过数组下标继续取值
+  assert(safeMap['class'].list.length == 2);
+  assert(safeMap['class'][0]['name'].value == 'class 1');
 
-    // {},[],0,'',null 都会被判断为空
-    assert(safeMap['class'][0]['tag'].isEmpty());
-    assert(safeMap['class'][1].isEmpty());
+  // {},[],0,'',null 都会被判断为空
+  assert(safeMap['class'][0]['tag'].isEmpty());
+  assert(safeMap['class'][1].isEmpty());
 
-    // 越界也会返回SafeMap(null)，判断isEmpty为true
-    assert(safeMap['class'][2].isEmpty());
-  });
+  // 越界也会返回SafeMap(null)，判断isEmpty为true
+  assert(safeMap['class'][2].isEmpty());
 }
